561a4734d1259d0ba4925d2be2ea15149f2e10a4
mallik0714 <mallikarjun.milkuri@hecta.co>
2026-07-11 13:29:24 +0530

    Add topic progress tracker

--- pipeline.js
+++ pipeline.js
@@ -162,7 +162,7 @@ Import Services
 */
 
 // const { getRandomTopic } = require("./services/topicService");
-const { getNextTopic } = require("./topicService");
+const { getNextTopic } = require("./services/topicService");
 const { generateScript } = require("./services/scriptService");
 const { splitIntoSentences } = require("./services/sentenceService");
 const { searchClips } = require("./services/clipSearchService");
